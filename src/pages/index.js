9c9122b90b6ced5eeec421496e2dd5c460cd82d6
Adil Arif <adilarif1@icloud.com>
2021-12-01 18:03:49 +0000

    merge

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,18 @@
 import React from "react"
+import Layout from "./components/Layout"
 
 export default function Home() {
   return (
 
+    <Layout>
     <section>
       <div>
         <h2>Design</h2>
-        <h3>Develop& Deploy</h3>
+        <h3>Develop & Deploy</h3>
         <p>Data Analyst, UX designer & web developer based in Glasgow</p>
       </div>
     </section>
-
+    </Layout>
 
 
   )
---
+++ src/pages/projects/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Layout from '../components/Layout'
+
+export default function Home() {
+    return (
+        <Layout>
+        <section>
+        <div>
+        <h2>Portfolio</h2>
+        <h3>Projects & Websites I've created</h3>
+        <p>Data Analyst, UX designer & web developer based in Glasgow</p>
+        </div>
+        </section>
+        </Layout>
+    )
+}
