dcf8520bab4479ee57a9e901379b8930082ea4f4
Adil Arif <adilarif1@icloud.com>
2021-12-01 23:57:47 +0000

    merge

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,19 @@
 import React from "react"
+import { Link } from "gatsby"
 import Layout from "./components/Layout"
-
+import {header, btn} from "../styles/home.module.css"
 export default function Home() {
   return (
 
     <Layout>
-    <section>
+    <section className={header}>
       <div>
         <h2>Design</h2>
         <h3>Develop & Deploy</h3>
         <p>Data Analyst, UX designer & web developer based in Glasgow</p>
+        <Link className={btn} to="/projects">My Portfolio Projects</Link>
       </div>
+      <img src="/me.png" alt="site banner me"  style={{maxWidth:'100%'}}></img>
     </section>
     </Layout>
 
--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import Layout from '../components/Layout'
-
+import {portfolio} from "../../styles/projects.module.css"
 export default function Home() {
     return (
         <Layout>
         <section>
-        <div>
+        <div className={portfolio}>
         <h2>Portfolio</h2>
         <h3>Projects & Websites I've created</h3>
         <p>Data Analyst, UX designer & web developer based in Glasgow</p>
