a2070018cc1dbbd7f9f844103791de16935e3cbd
Adil Arif <adilarif1@icloud.com>
2022-01-20 20:18:30 +0000

    “fixing”

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import React from "react"
 import { graphql,Link } from "gatsby"
 import Layout from "./components/Layout"
-import {header, btn} from "../styles/home.module.css"
+import {header, btn, flexcontainer, flexitemleft, flexitemright} from "../styles/home.module.css"
 
 import { StaticImage } from "gatsby-plugin-image"
 export default function Home({data}) {
@@ -10,16 +10,13 @@ export default function Home({data}) {
   return (
   
     <Layout>
-    <section className={header}>
-      <div>
-        <h2>Design</h2>
+    <div className={flexcontainer}>
+  <div className={flexitemleft}><StaticImage src="../../src/images/me.png" alt="site banner me" placeholder="none" loading="eager" width={350} height={400} layout="constrained" aspectRatio= "21/9"  /></div>
+  <div className={flexitemright}><h2>Design</h2>
         <h3>Develop & Deploy</h3>
         <p>Data Analyst, UX designer & web developer based in Glasgow</p>
-        <Link className={btn} to="../projects">My Portfolio Projects</Link>
-      </div>
-      <StaticImage src="../../src/images/me.png" alt="site banner me" placeholder="none" loading="eager" layout="constrained" aspectRatio= "21/9"  />
-      {/*<StaticImage fluid={data.file.childrenImageSharp.fluid.src}/>*/}
-    </section>
+        <Link className={btn} to="../projects">My Portfolio Projects</Link></div>
+</div>
     </Layout>
 
 
