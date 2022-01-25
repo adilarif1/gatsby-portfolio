1609ef2ad53c08481be0ab8ab4874e3931cc3575
Adil Arif <adilarif1@icloud.com>
2022-01-25 17:55:59 +0000

    update

--- src/pages/about.js
+++ src/pages/about.js
@@ -6,8 +6,7 @@ export default function about() {
         <Layout>
         <div>
             <h1>About Page</h1>
-            <p>Hi, I am Adil a developer in many areas of IT. I am passionate about mobile development, web development, AI and machine learning. </p>
-            <p>I am a jack of all trades sort of guy that likes to help people. I hold a BSc Hons in Web and Mobile development and a MSc in Big Data.</p>
+            <p>Hi, I am Adil a developer in many areas of IT. I am passionate about mobile development, web development, AI and machine learning. I am a jack of all trades sort of guy that likes to help people. I hold a BSc Hons in Web and Mobile development and a MSc in Big Data. Currently in market for job opporutunites whether it may be full-time, part-time of contracted freelancing. </p>
             
         </div>
         </Layout>
